7e24d89df3c70d89f6018651352258852f5f6aa8
Esaú García Sánchez-Torija <egasato@protonmail.com>
2019-05-05 18:03:08 +0200

    Add PciTest class with constructor and getVendorId test cases

---
+++ pci/src/test/java/de/tum/in/net/ixy/pci/test/PciTest.java
@@ -0,0 +1,161 @@
+package de.tum.in.net.ixy.pci.test;
+
+import de.tum.in.net.ixy.pci.Pci;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Objects;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.StringRegularExpression.matchesRegex;
+
+/** Checks the class {@link Pci}. */
+@Slf4j
+@DisplayName("PCI device access (read & write)")
+class PciTest {
+
+	/** The expected vendor id of the Virtio devices. */
+	private static final short EXPECTED_VENDOR = 0x1af4;
+
+	/** The expected device id of the Virtio devices. */
+	private static final short EXPECTED_DEVICE = 0x1000;
+
+	/** The expected class id of the Virtio devices. */
+	private static final byte EXPECTED_CLASS = 0x02;
+
+	/**
+	 * Checks that the vendor id of the Virtio devices is correct.
+	 *
+	 * @param pciDevice A PCI device.
+	 */
+	@ParameterizedTest(name = "[static] Vendor id of {0} should be " + EXPECTED_VENDOR + " (Red Hat, Inc)")
+	@MethodSource("pciSource")
+	void getVendorId(@NonNull final String pciDevice) {
+		try {
+			assertEquals(EXPECTED_VENDOR, Pci.getVendorId(pciDevice), "vendor id should be correct");
+		} catch (FileNotFoundException e) {
+			log.warn("The PCI device does not exist", e);
+		} catch (IOException e) {
+			log.error("Unexpected I/O error", e);
+		}
+	}
+
+//
+//	/**
+//	 * Tests the device id of the PCI devices.
+//	 *
+//	 * @param pci a PCI device
+//	 */
+//	@ParameterizedTest(name = "device id of {0} should be " + EXPECTED_DEVICE + " (Virtio network device)")
+//	@MethodSource("pciSource")
+//	void getDeviceId(@NonNull final Pci pci) {
+//		assertEquals(EXPECTED_DEVICE, pci.getDeviceId(), "device id should be correct");
+//	}
+
+//	/**
+//	 * Tests the class id of the PCI devices.
+//	 *
+//	 * @param pci a PCI device
+//	 */
+//	@ParameterizedTest(name = "class id of {0} should be " + EXPECTED_CLASS + " (Network controller)")
+//	@MethodSource("pciSource")
+//	void getClassId(@NonNull final Pci pci) {
+//		assertEquals(EXPECTED_CLASS, pci.getClassId(), "class id should be correct");
+//	}
+
+	/** Checks the non-static methods of the class {@link Pci}. */
+	static class NonStatic {
+
+		/** Checks that creating a new instance with an invalid PCI device throws a {@link FileNotFoundException}. */
+		@Test
+		@DisplayName("Invalid PCI device throws FileNotFoundException")
+		void constructorException() {
+			val exception = assertThrows(FileNotFoundException.class, () -> new Pci(""));
+			assertThat(exception.getMessage(), matchesRegex("^.*config \\(No such file or directory\\)$"));
+		}
+
+		/**
+		 * Checks that the vendor id of the Virtio devices is correct.
+		 *
+		 * @param pci A {@link Pci} instance.
+		 */
+		@ParameterizedTest(name = "Vendor id of {0} should be " + EXPECTED_VENDOR + " (Red Hat, Inc)")
+		@MethodSource("pciSource")
+		void getVendorId(@NonNull final Pci pci) throws IOException {
+			assertEquals(EXPECTED_VENDOR, pci.getVendorId(), "vendor id should be correct");
+		}
+
+		/**
+		 * Source of {@link Pci} instances.
+		 * <p>
+		 * This source uses the {@link PciTest#pciSource()} as input and tries to create a {@link Pci} instance for each
+		 * PCI device. If the instantiation throws a {@link FileNotFoundException}, that device is discarded and is not
+		 * included in the output stream.
+		 *
+		 * @return A {@link Stream} of valid {@link Pci} instances.
+		 */
+		@NotNull
+		private static Stream<Pci> pciSource() {
+			return PciTest.pciSource()
+					.map(PciTest::newPci)
+					.filter(Objects::nonNull);
+		}
+
+	}
+
+	/////////////////////////////////////////////////// STATIC UTILS ///////////////////////////////////////////////////
+
+	/**
+	 * Source of PCI devices.
+	 * <p>
+	 * Reads the environment variable {@code IXY_VIRTIO_COUNT} and creates a {@link Stream<String>} containing all the
+	 * names.
+	 * <p>
+	 * This method does not filter the PCI devices by checking the existence of the devices themselves, it only discards
+	 * {@code null} or empty devices. Therefore, there is no guarantee that creating an instance of {@link Pci} will not
+	 * throw a {@link FileNotFoundException}.
+	 *
+	 * @return A {@link Stream} of PCI devices.
+	 */
+	@NotNull
+	private static Stream<String> pciSource() {
+		val count = System.getenv("IXY_VIRTIO_COUNT");
+		return IntStream.range(1, 1 + Integer.parseInt(count))
+				.mapToObj(i -> System.getenv("IXY_VIRTIO_ADDR_" + i))
+				.filter(Objects::nonNull)
+				.filter(it -> !it.isEmpty());
+	}
+
+	///////////////////////////////////////////////// INTERNAL METHODS /////////////////////////////////////////////////
+
+	/**
+	 * Creates a {@link Pci} instance and handles any checked exception.
+	 * <p>
+	 * If the given PCI device does not exist, {@code null} is returned.
+	 *
+	 * @param pciDevice The PCI device.
+	 * @return The {@link Pci} instance.
+	 */
+	private static Pci newPci(final String pciDevice) {
+		try {
+			return new Pci(pciDevice);
+		} catch (FileNotFoundException e) {
+			log.warn("Could not create PCI instance", e);
+		}
+		return null;
+	}
+
+}
