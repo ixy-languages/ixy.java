a3b95de44f2ce9a1032d86e3dbced9585a77175d
Esaú García Sánchez-Torija <egasato@protonmail.com>
2019-05-05 18:00:16 +0200

    Add Java 9 module specification

--- pci/src/main/java/module-info.java
+++ pci/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 // Define the module name
 module ixy.pci {
-    requires lombok;               // Lombok annotations
-    requires annotations;          // JetBrains annotations
-    requires org.slf4j;            // Simple Logging Facade for Java
-    exports de.tum.in.net.ixy.pci; // Export the PCI module
+	requires lombok;                // Lombok annotations
+	requires annotations;           // JetBrains annotations
+	requires org.slf4j;             // Simple Logging Facade for Java
+	exports  de.tum.in.net.ixy.pci; // Export the PCI module
 }
