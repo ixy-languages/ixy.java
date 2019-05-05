d164630603981fff5e11e7ca8e6d181248deffb0
Esaú García Sánchez-Torija <egasato@protonmail.com>
2019-05-05 18:00:55 +0200

    Add Pci class with getVendorId implementation

---
+++ pci/src/main/java/de/tum/in/net/ixy/pci/Pci.java
@@ -0,0 +1,141 @@
+package de.tum.in.net.ixy.pci;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.channels.ReadableByteChannel;
+
+/**
+ * PCI static utils with instantiation support.
+ * <p>
+ * This class allows reading and writing to arbitrary PCI name's resources and also instantiation, so that all the
+ * reading/writing targets the same PCI name. This is useful to reduce the verbosity of the code that only needs to
+ * read/write from/to a specific PCI name.
+ * <p>
+ * If the static methods are used, a disposable non-direct {@link ByteBuffer} is allocated to read/write from/to the
+ * file, otherwise a direct {@link ByteBuffer} big enough to read all the different PCI properties is allocated.
+ * <p>
+ * The required resources when instantiating are the collection of all resources used by the static methods, which are
+ * {@code config}.
+ *
+ * @author Esaú García Sánchez-Torija
+ * @see ByteBuffer
+ */
+@Slf4j
+public final class Pci {
+
+	/** Path format string used to build the path to a PCI device. */
+	private static final String PCI_PATH_FMT = "/sys/bus/pci/devices/%s";
+
+	/** Path format string used to build the path to a resource of a PCI device. */
+	private static final String PCI_RES_PATH_FMT = PCI_PATH_FMT + "/%s";
+
+	/** The name of the PCI device being manipulated. */
+	@Getter
+	private String name;
+
+	/** The {@link RandomAccessFile} instance used to access the resource {@code config} of PCI device. */
+	private RandomAccessFile config;
+
+	/** Direct {@link ByteBuffer} used to read/write from/to the resources of the PCI device. */
+	private final ByteBuffer buffer = ByteBuffer.allocateDirect(12).order(ByteOrder.nativeOrder());
+
+	/**
+	 * Allocates the resources needed to read/write from/to the PCI device as fast as possible.
+	 *
+	 * @param pciDevice The name of the PCI device.
+	 * @throws FileNotFoundException If the specified PCI device does not exist or any of its required resources.
+	 */
+	public Pci(@NonNull final String pciDevice) throws FileNotFoundException {
+		name = pciDevice;
+		config = new RandomAccessFile(String.format(PCI_RES_PATH_FMT, name, "config"), "r");
+	}
+
+	/**
+	 * Reads the vendor id.
+	 * <p>
+	 * This method uses the previously allocated direct {@link ByteBuffer} {@link #buffer} and reads the contents of the
+	 * resource {@code config} from the PCI device.
+	 *
+	 * @return The vendor id of the PCI device.
+	 * @throws FileNotFoundException If the given {@code pciDevice} or its resource {@code config} do not exist.
+	 * @throws IOException           If an I/O error occurs when calling {@link #getVendorId(ByteBuffer,
+	 *                               ReadableByteChannel)}.
+	 * @see #getVendorId(ByteBuffer, ReadableByteChannel)
+	 */
+	public short getVendorId() throws IOException {
+		log.trace("Reading vendor id of PCI device {}", name);
+		return getVendorId(buffer.position(0), config.getChannel().position(0));
+	}
+
+	/**
+	 * Returns the {@link String} representation of this PCI device.
+	 * <p>
+	 * The {@link String} representation of this PCI device is nothing more than the path used to access all its
+	 * resources. This method has been implemented so that an instance can be used to generate the display text of a
+	 * test case.
+	 *
+	 * @return The path of the PCI device.
+	 */
+	@Override
+	public String toString() {
+		return String.format(PCI_PATH_FMT, name);
+	}
+
+	////////////////////////////////////////////////// STATIC METHODS //////////////////////////////////////////////////
+
+	/**
+	 * Given the name of a PCI device, reads its vendor id.
+	 * <p>
+	 * This method creates a disposable non-direct {@link ByteBuffer} and reads the contents of the resource {@code
+	 * config} from the given PCI device.
+	 *
+	 * @param pciDevice The name of the PCI device.
+	 * @return The vendor id of the PCI device.
+	 * @throws FileNotFoundException If the given {@code pciDevice} or its resource {@code config} do not exist.
+	 * @throws IOException           If an I/O error occurs when calling {@link #getVendorId(ByteBuffer,
+	 *                               ReadableByteChannel)}.
+	 * @see #getVendorId(ByteBuffer, ReadableByteChannel)
+	 */
+	public static short getVendorId(final String pciDevice) throws FileNotFoundException, IOException {
+		log.trace("Reading vendor id of PCI device {}", pciDevice);
+		val path = String.format(PCI_RES_PATH_FMT, pciDevice, "config");
+		val file = new RandomAccessFile(path, "r");
+		val buffer = ByteBuffer.allocate(2).order(ByteOrder.nativeOrder());
+		return getVendorId(buffer, file.getChannel());
+	}
+
+	///////////////////////////////////////////////// INTERNAL METHODS /////////////////////////////////////////////////
+
+	/**
+	 * Reads the required bytes to get the vendor id.
+	 * <p>
+	 * This method assumes that the {@code buffer} has enough space to read the required amount of bytes, uses the
+	 * correct byte order so that the read data is meaningful, and that the {@code channel} has enough remaining bytes,
+	 * it is positioned in the correct position and it reads from the correct file.
+	 * <p>
+	 * This method exists only to reduce the amount of code used in the rest of publicly available methods. This method
+	 * should be inline, but Java does not support such specifier.
+	 *
+	 * @param buffer  The {@link ByteBuffer} where the bytes will be written to.
+	 * @param channel The {@link ReadableByteChannel} where the bytes will be read from.
+	 * @return The vendor id.
+	 * @throws IOException If an I/O error occurs when reading the bytes from the {@code channel} and writing them to
+	 *                     the {@code buffer}.
+	 */
+	private static short getVendorId(final ByteBuffer buffer, final ReadableByteChannel channel) throws IOException {
+		val bytes = channel.read(buffer);
+		if (bytes != 2) {
+			log.warn("Could't read the exact amount of bytes needed to read the vendor id");
+		}
+		return buffer.getShort(0);
+	}
+
+}
