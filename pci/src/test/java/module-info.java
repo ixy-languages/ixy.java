0b7e3bf5a50df73a9387ecc9e417c578aff6e5c1
Esaú García Sánchez-Torija <egasato@protonmail.com>
2019-05-10 22:56:35 +0200

    Remove unused module

--- pci/src/test/java/module-info.java
+++ pci/src/test/java/module-info.java
@@ -5,5 +5,4 @@ module ixy.pci.test {
 	requires annotations;              // JetBrains annotations
 	requires org.junit.jupiter.api;    // JUnit 5 annotations API
 	requires org.junit.jupiter.params; // JUnit 5 parameterized test API
-	requires org.hamcrest;             // Hamcrest matchers
 }
