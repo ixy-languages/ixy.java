69cb4f7e0cf0787b5dd40a0cc03c736d8cf004b3
Esaú García Sánchez-Torija <egasato@protonmail.com>
2019-05-10 22:58:39 +0200

    Fix comment

--- pci/src/main/java/module-info.java
+++ pci/src/main/java/module-info.java
@@ -3,5 +3,5 @@ module ixy.pci {
 	requires lombok;                // Lombok annotations
 	requires annotations;           // JetBrains annotations
 	requires org.slf4j;             // Simple Logging Facade for Java
-	exports  de.tum.in.net.ixy.pci; // Export the PCI module
+	exports  de.tum.in.net.ixy.pci; // Export the pci package
 }
