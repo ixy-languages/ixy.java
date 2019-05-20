6838c3ec997e7908fb10a059acee6415de0ac547
Esaú García Sánchez-Torija <egasato@protonmail.com>
2019-05-20 02:07:35 +0200

    Refactor MemoryPool to implement the Collection interface

--- memory/src/main/java/de/tum/in/net/ixy/memory/MemoryPool.java
+++ memory/src/main/java/de/tum/in/net/ixy/memory/MemoryPool.java
@@ -1,23 +1,37 @@
 package de.tum.in.net.ixy.memory;
 
 import java.util.ArrayDeque;
+import java.util.Collection;
 import java.util.Deque;
+import java.util.Iterator;
+import java.util.Spliterator;
 import java.util.TreeMap;
+import java.util.function.Consumer;
+import java.util.function.IntFunction;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+import org.jetbrains.annotations.NotNull;
 
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.val;
 import lombok.extern.slf4j.Slf4j;
 
 /**
  * Represents a memory region reserved for a specific number of {@link PacketBuffer}s.
  * <p>
+ * This class implements the {@link Comparable} and {@link Collection} interfaces to better integrate with Java
+ * ecosystem, although it is not entirely compliant. This class only implements the methods that allow adding elements,
+ * that is to say, the methods that allow to add back again the free {@link PacketBuffers}.
+ * <p>
  * The main goal of this class is to enable easy management of {@link PacketBuffer} instances.
  */
 @Slf4j
-public final class MemoryPool implements Comparable<MemoryPool> {
+public final class MemoryPool implements Comparable<MemoryPool>, Collection<PacketBuffer> {
 
 	/** Stores all the memory pools ever created. */
-	public static final TreeMap<Integer, MemoryPool> memoryPools = new TreeMap<>();
+	public static final TreeMap<Integer, MemoryPool> pools = new TreeMap<>();
 
 	/**
 	 * Given an {@code id}, computes one that is not already being used by another memory pool.
@@ -26,7 +40,7 @@ public final class MemoryPool implements Comparable<MemoryPool> {
 	 * @return An id that is not being used.
 	 */
 	private static int getValidId(int id) {
-		while (memoryPools.containsKey(id)) {
+		while (pools.containsKey(id)) {
 			id += 1;
 		}
 		if (BuildConstants.DEBUG) log.trace("Found a valid memory pool id {}", id);
@@ -34,45 +48,55 @@ public final class MemoryPool implements Comparable<MemoryPool> {
 	}
 
 	/**
-	 * Adds a memory pool to {@link #memoryPools}.
+	 * Adds a memory pool to {@link #pools}.
+	 * <p>
+	 * If the memory pool id is already in the {@link #pools} collection, then a new id is generated and the instance
+	 * is updated.
 	 * 
-	 * @param memoryPool The memory pool to add.
+	 * @param mempool The memory pool to add.
+	 * @see MemoryPool#getValidId(int)
 	 */
-	public static void addMemoryPool(final MemoryPool memoryPool) {
-		if (BuildConstants.DEBUG) log.trace("Adding memory pool");
-		val id = getValidId(memoryPool.id);
-		memoryPool.id = id;
-		memoryPools.put(id, memoryPool);
+	public static boolean addMempool(final MemoryPool mempool) {
+		if (BuildConstants.DEBUG) log.trace("Adding a memory pool");
+		if (mempool == null) {
+			return false;
+		}
+		val id = getValidId(mempool.id);
+		mempool.id = id;
+		pools.put(id, mempool);
+		return true;
 	}
 
 	/** The unique identifier of the memory pool. */
 	@Getter
 	private int id;
-	
+
 	/** The base address of the memory pool. */
 	@Getter
 	private long baseAddress;
 
-	/** The size of the buffer. */
+	/** The size of the packet buffers. */
 	@Getter
-	private int bufferSize;
+	private int packetBufferSize;
 
 	/** The number of entries the pool has. */
 	@Getter
 	private int entrySize;
 
-	/** double ended queue with a bunch a pre-allocated {@link PacketBuffer} instances. */
+	/** Bouble ended queue with a bunch a pre-allocated {@link PacketBuffer} instances. */
 	private Deque<PacketBuffer> buffers;
 
 	/**
-	 * Creates an instance of the memory pool.
+	 * Creates an instance an empty instance.
 	 * <p>
-	 * This constructor automatically keeps track of the created instances by adding them to the list {@link
-	 * #memoryPools}.
+	 * In order to use the instance, all the {@link PacketBuffer}s must be allocated.
+	 * <p>
+	 * This constructor automatically keeps track of the created instances by adding them to the list {@link #pools}.
 	 * 
 	 * @param address The base address of the memory pool.
 	 * @param size    The size of each buffer inside the memory pool.
 	 * @param entries The number of entries to allocate.
+	 * @see #allocate()
 	 */
 	public MemoryPool(final long address, final int size, final int entries) {
 		if (BuildConstants.DEBUG) {
@@ -80,34 +104,36 @@ public final class MemoryPool implements Comparable<MemoryPool> {
 			log.trace("Creating memory pool with {} entries, a total size of {} bytes @ 0x{}", entries, size, xaddress);
 		}
 		baseAddress = address;
-		bufferSize = size;
+		packetBufferSize = size;
 		entrySize = entries;
-		id = memoryPools.isEmpty() ? 0 : getValidId(memoryPools.lastKey());
-		memoryPools.put(id, this);
-		if (BuildConstants.DEBUG) log.info("There are {} memory pools", memoryPools.size());
+		id = pools.isEmpty() ? 0 : getValidId(pools.lastKey());
+		pools.put(id, this);
+		if (BuildConstants.DEBUG) log.info("There are {} memory pools", pools.size());
 	}
 
-	/** Fills the collection {@link #buffers} with {@link #entrySize} {@link PacketBuffer} instances. */
-	public void allocatePacketBuffers() {
+	/** Pre-allocates all the {@link PacketBuffer}s. */
+	public void allocate() {
 		if (BuildConstants.DEBUG) log.trace("Allocating packet buffers");
 		buffers = new ArrayDeque<PacketBuffer>(entrySize);
 		for (var i = 0; i < entrySize; i += 1) {
-			val virt = baseAddress + i * bufferSize;
+			val virt = baseAddress + i * packetBufferSize;
 			val buffer = new PacketBuffer(virt);
 			buffer.setPhysicalAddress(MemoryUtils.virt2phys(virt));
 			buffer.setSize(0);
-			buffers.add(buffer);
+			buffers.push(buffer);
 		}
 	}
 
 	/**
 	 * Returns a free pre-allocated packet buffer instance.
 	 * <p>
-	 * If the collection storing the free buffers is empty, then a dummy instance is returned.
+	 * If there are no more free {@link PacketBuffer}s available, then an empty instance is returned.
 	 * 
 	 * @return A free packet buffer or a dummy instance.
+	 * @see PacketBuffer#empty()
 	 */
-	public PacketBuffer getFreePacketBuffer() {
+	@NotNull
+	public PacketBuffer pop() {
 		if (BuildConstants.DEBUG) {
 			log.trace("Obtaining a free packet buffer");
 			if (buffers.isEmpty()) {
@@ -120,29 +146,161 @@ public final class MemoryPool implements Comparable<MemoryPool> {
 		}
 	}
 
-	/**
-	 * Frees a packet buffer, that is to say, adds it to the collection of free packet buffers.
-	 * <p>
-	 * This method will do nothing if the size of {@link #buffers} has already reached {@link #entrySize}.
-	 * 
-	 * @param buffer The packet buffer to free.
-	 */
-	public void freePacketBuffer(final PacketBuffer buffer) {
-		if (buffers.size() >= entrySize) {
-			log.warn("The packet buffer cannot be freed because the stack is full");
-		} else {
-			buffers.push(buffer);
-		}
-	}
-
 	//////////////////////////////////////////////// OVERRIDEN METHODS /////////////////////////////////////////////////
 
-	/**
-	 * {@inheritDoc}
-	 */
+	/** {@inheritDoc} */
 	@Override
-	public int compareTo(MemoryPool memoryPool) {
-		return id - memoryPool.id;
+	public int compareTo(@NonNull final MemoryPool mempool) {
+		if (BuildConstants.DEBUG) log.trace("Comparing with another MemoryPool");
+		return id - mempool.id;
+	}
+
+	/** {@inheritDoc} */
+	public int size() {
+		return buffers.size();
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public boolean isEmpty() {
+		if (BuildConstants.DEBUG) log.trace("Checking if MemoryPool is empty");
+		return buffers.isEmpty();
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public boolean add(final PacketBuffer packetBuffer) {
+		if (BuildConstants.DEBUG) log.trace("Adding PacketBuffer to the memory pool");
+		if (packetBuffer == null) {
+			if (BuildConstants.DEBUG) log.warn("Skipping invalid PacketBuffer that is null");
+			return false;
+		} else if (buffers.size() >= entrySize) {
+			if (BuildConstants.DEBUG) log.warn("Memory pool queue is already full, cannot add free PacketBuffer");
+			return false;
+		}
+		buffers.add(packetBuffer);
+		return true;
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public boolean addAll(final Collection<? extends PacketBuffer> packetBuffers) {
+		if (BuildConstants.DEBUG) log.trace("Adding more than one PacketBuffer to the memory pool");
+		if (packetBuffers == null) {
+			return false;
+		}
+		val amount = packetBuffers.size();
+		val size = buffers.size();
+		if (amount <= 0) {
+			return false;
+		} else if (size >= entrySize) {
+			if (BuildConstants.DEBUG) log.warn("Memory pool queue is already full, cannot add any free PacketBuffer");
+			return false;
+		}
+		var remaining = entrySize - size;
+		if (BuildConstants.DEBUG && amount > remaining) {
+			log.warn("Memory pool queue will be full, cannot add all free PacketBuffers");
+		}
+		for (val packetBuffer : packetBuffers) {
+			if (packetBuffer != null) {
+				buffers.add(packetBuffer);
+				if (--remaining == 0) break;
+			} else if (BuildConstants.DEBUG) {
+				log.warn("Skipping invalid PacketBuffer that is null");
+			}
+		}
+		return size != buffers.size();
+	}
+
+
+	/** {@inheritDoc} */
+	@Override
+	public Iterator<PacketBuffer> iterator() {
+		throw new UnsupportedOperationException("only methods that return or add packet buffers are implemented");
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public void forEach(Consumer<? super PacketBuffer> action) {
+		throw new UnsupportedOperationException("only methods that return or add packet buffers are implemented");
+	}
+
+	/** {@inheritDoc} */
+	@Override
+    public Spliterator<PacketBuffer> spliterator() {
+		throw new UnsupportedOperationException("only methods that return or add packet buffers are implemented");
+	}
+
+
+	/** {@inheritDoc} */
+	@Override
+	public boolean contains(final Object o) {
+		throw new UnsupportedOperationException("only methods that return or add packet buffers are implemented");
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public boolean containsAll(final Collection<?> c) {
+		throw new UnsupportedOperationException("only methods that return or add packet buffers are implemented");
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public Object[] toArray() {
+		throw new UnsupportedOperationException("only methods that return or add packet buffers are implemented");
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public <T> T[] toArray(final T[] a) {
+		throw new UnsupportedOperationException("only methods that return or add packet buffers are implemented");
+	}
+
+	@Override
+	public <T> T[] toArray(final IntFunction<T[]> generator) {
+		throw new UnsupportedOperationException("only methods that return or add packet buffers are implemented");
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public boolean remove(final Object o) {
+		throw new UnsupportedOperationException("only methods that return or add packet buffers are implemented");
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public boolean removeIf(Predicate<? super PacketBuffer> filter) {
+		throw new UnsupportedOperationException("only methods that return or add packet buffers are implemented");
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public boolean removeAll(final Collection<?> packetBuffers) {
+		throw new UnsupportedOperationException("only methods that return or add packet buffers are implemented");
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public boolean retainAll(final Collection<?> packetBuffers) {
+		throw new UnsupportedOperationException("only methods that return or add packet buffers are implemented");
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public Stream<PacketBuffer> stream() {
+		throw new UnsupportedOperationException("only methods that return or add packet buffers are implemented");
+	}
+	
+	/** {@inheritDoc} */
+	@Override
+	public Stream<PacketBuffer> parallelStream() {
+		throw new UnsupportedOperationException("only methods that return or add packet buffers are implemented");
+    }
+
+	/** {@inheritDoc} */
+	@Override
+	public void clear() {
+		throw new UnsupportedOperationException("only methods that return or add packet buffers are implemented");
 	}
 
 }
--- memory/src/test/java/de/tum/in/net/ixy/memory/test/MemoryPoolTest.java
+++ memory/src/test/java/de/tum/in/net/ixy/memory/test/MemoryPoolTest.java
@@ -5,9 +5,11 @@ import de.tum.in.net.ixy.memory.MemoryUtils;
 import de.tum.in.net.ixy.memory.PacketBuffer;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.function.IntFunction;
 import java.util.stream.IntStream;
 
 import org.junit.jupiter.api.AfterAll;
@@ -45,16 +47,14 @@ class MemoryPoolTest {
 
 	/** Holds an instance of {@link MemoryPool} that will be used to test. */
 	@NotNull
-	private static MemoryPool memoryPool;
+	private static MemoryPool mempool;
 
-	//////////////////////////////////////////////// ASSERTION MESSAGES ////////////////////////////////////////////////
-
-	private static final String MSG_NOT_NULL = "should not be null";
-	private static final String MSG_NULL     = "should be null";
-	private static final String MSG_NOT_0    = "should not be 0";
-	private static final String MSG_UNIQUE   = "should be unique";
-
-	/** Allocates a region of memory, assigns it to {@link #virtual} and creates an instance of {@link PacketBuffer}. */
+	/**
+	 * Allocates a region of memory, assigns it to {@link #virtual} and creates an instance of {@link PacketBuffer}.
+	 * 
+	 * @see MemoryPool#MemoryPool(long, int, int)
+	 * @see MemoryPool#allocate()
+	 */
 	@BeforeAll
 	static void allocate() {
 		size = (int) MemoryUtils.getHugepagesize();
@@ -66,102 +66,182 @@ class MemoryPoolTest {
 		virtual = MemoryUtils.allocate(size, false);
 		numEntries = perfectSizes[(int) (Math.random() * perfectSizes.length)];
 		size /= numEntries;
-		memoryPool = new MemoryPool(virtual, size, numEntries);
-		memoryPool.allocatePacketBuffers();
+		mempool = new MemoryPool(virtual, size, numEntries);
+		mempool.allocate();
+	}
+
+	/**
+	 * Checks that not all the methods from the {@link Collection} interface are implemented.
+	 * 
+	 * @see MemoryPool#iterator()
+	 * @see MemoryPool#forEach(java.util.function.Consumer)
+	 * @see MemoryPool#spliterator()
+	 * @see MemoryPool#contains(Object)
+	 * @see MemoryPool#containsAll(java.util.Collection)
+	 * @see MemoryPool#toArray()
+	 * @see MemoryPool#toArray(Object[])
+	 * @see MemoryPool#toArray(IntFunction)
+	 * @see MemoryPool#remove(Object)
+	 * @see MemoryPool#removeIf(java.util.function.Predicate)
+	 * @see MemoryPool#removeAll(java.util.Collection)
+	 * @see MemoryPool#clear()
+	 * @see MemoryPool#stream()
+	 * @see MemoryPool#parallelStream()
+	 */
+	@Test
+	@Order(0)
+	@DisplayName("Unsupported methods throw")
+	void unsupported() {
+		assertThrows(UnsupportedOperationException.class, () -> mempool.iterator());
+		assertThrows(UnsupportedOperationException.class, () -> mempool.forEach(null));
+		assertThrows(UnsupportedOperationException.class, () -> mempool.spliterator());
+		assertThrows(UnsupportedOperationException.class, () -> mempool.contains(null));
+		assertThrows(UnsupportedOperationException.class, () -> mempool.containsAll(null));
+		assertThrows(UnsupportedOperationException.class, () -> mempool.toArray());
+		assertThrows(UnsupportedOperationException.class, () -> mempool.toArray((PacketBuffer[]) null));
+		assertThrows(UnsupportedOperationException.class, () -> mempool.toArray((IntFunction<PacketBuffer[]>) null));
+		assertThrows(UnsupportedOperationException.class, () -> mempool.remove(null));
+		assertThrows(UnsupportedOperationException.class, () -> mempool.removeIf(null));
+		assertThrows(UnsupportedOperationException.class, () -> mempool.removeAll(null));
+		assertThrows(UnsupportedOperationException.class, () -> mempool.retainAll(null));
+		assertThrows(UnsupportedOperationException.class, () -> mempool.clear());
+		assertThrows(UnsupportedOperationException.class, () -> mempool.stream());
+		assertThrows(UnsupportedOperationException.class, () -> mempool.parallelStream());
 	}
 
 	/** Holds temporarily the packet buffers. */
-	private static List<PacketBuffer> buffers;
+	private static ArrayList<PacketBuffer> buffers;
 
 	/**
-	 * Checks that the packet buffers can be obtained from the memory pool.
+	 * Checks that the {@link PacketBuffer}s can be obtained from the memory pool.
 	 * 
-	 * @see MemoryPool#getFreePacketBuffer()
+	 * @see MemoryPool#pop()
 	 */
 	@Test
 	@Order(0)
 	@DisplayName("Packet buffers can be obtained from the memory pool")
-	void getFreePacketBuffer() {
+	void pop() {
 		buffers = new ArrayList<>(numEntries);
-		var size = memoryPool.size();
-		while (size > 0) {
-			val buffer = memoryPool.getFreePacketBuffer();
-			val tmpSize = memoryPool.size();
+		var size = mempool.size();
+		while (size > 0 && !mempool.isEmpty()) {
+			val buffer = mempool.pop();
+			val tmpSize = mempool.size();
 			buffers.add(buffer);
-			assertEquals(size - 1, tmpSize, "the size should be 1 unit less");
+			assertEquals(size - 1, tmpSize);
 			size = tmpSize;
 		}
 	}
 
-	/** Holds the virtual addresses of the packet buffers. */
+	/** Holds the virtual addresses of the {@link PacketBuffer}s. */
 	private static Set<Long> virtualAddresses = new HashSet<>();
 
-	/** Holds the physical addresses of the packet buffers. */
+	/** Holds the physical addresses of the {@link PacketBuffer}s. */
 	private static Set<Long> physicalAddresses = new HashSet<>();
 
-	/** Checks that the packet buffers have the correct properties. */
+	/** Checks that the {@link PacketBuffer}s have the correct properties. */
 	@Test
 	@Order(1)
 	@DisplayName("The packet buffers are correctly initialized")
 	void packetBufferTest() {
 		for (val packetBuffer : buffers) {
-			assertNotNull(packetBuffer, MSG_NOT_NULL);
+			assertNotNull(packetBuffer);
 			val vaddress = packetBuffer.getBaseAddress();
 			val paddress = MemoryUtils.virt2phys(vaddress);
-			assertNotEquals(0, vaddress,               MSG_NOT_NULL);
-			assertNotEquals(0, paddress,               MSG_NOT_NULL);
-			assertEquals(0,    packetBuffer.getSize(), MSG_NOT_0);
-			assertFalse(virtualAddresses.contains(vaddress), MSG_UNIQUE);
-			assertFalse(virtualAddresses.contains(paddress), MSG_UNIQUE);
+			assertNotEquals(0, vaddress);
+			assertNotEquals(0, paddress);
+			assertEquals(0, packetBuffer.getSize());
+			assertFalse(virtualAddresses.contains(vaddress));
+			assertFalse(virtualAddresses.contains(paddress));
 			virtualAddresses.add(vaddress);
 			physicalAddresses.add(paddress);
 		}
 	}
 
-	/** Checks that the memory pool cannot provide more packet buffers than the pre-allocated amount. */
+	/**
+	 * Checks that the memory pool cannot provide more {@link PacketBuffer}s than the pre-allocated amount.
+	 * 
+	 * @see MemoryPool#pop()
+	 */
 	@Test
 	@Order(1)
 	@DisplayName("Obtaining more packets than available results in an empty packet")
 	void getFreePacketBuffer2() {
-		val packet = memoryPool.getFreePacketBuffer();
-		assertNotNull(packet, MSG_NOT_NULL);
-		assertEquals(0, packet.getBaseAddress(), MSG_NULL);
+		val packet = mempool.pop();
+		assertNotNull(packet);
+		assertEquals(0, packet.getBaseAddress());
 	}
 
-	/** Checks that the packet buffers can be freed. */
+	/**
+	 * Checks that the {@link PacketBuffer}s can be freed.
+	 * 
+	 * @see MemoryPool#add(PacketBuffer)
+	 */
 	@Test
 	@Order(2)
 	@DisplayName("The packet buffers can be freed")
-	void freePacketBuffer() {
-		var size = memoryPool.size();
+	void add() {
+		var size = mempool.size();
 		for (val packetBuffer : buffers) {
-			memoryPool.freePacketBuffer(packetBuffer);
-			val tmpSize = memoryPool.size();
-			assertEquals(size + 1, tmpSize, "the size should be 1 unit more");
+			assertTrue(mempool.add(packetBuffer));
+			val tmpSize = mempool.size();
+			assertEquals(size + 1, tmpSize);
 			size = tmpSize;
 		}
-		memoryPool.freePacketBuffer(PacketBuffer.empty());
-		assertEquals(size, memoryPool.size(), "the size should be the same");
-		assertThrows(NullPointerException.class, () -> memoryPool.freePacketBuffer(null));
+		assertFalse(mempool.add(PacketBuffer.empty()));
+		assertFalse(mempool.add(null));
+		assertEquals(size, mempool.size());
 	}
 
-	/** Checks that pools have different ids. */
+	/**
+	 * Checks that different memory pools have different ids.
+	 * 
+	 * @see MemoryPool#getId()
+	 * @see MemoryPool#compareTo(MemoryPool)
+	 * @see MemoryPool#addMempool(MemoryPool)
+	 */
 	@Test
 	@Order(2)
 	@DisplayName("Different pools have different ids")
-	void memoryPoolId() {
+	void mempoolId() {
 		val pool = new MemoryPool(0, 0, 0);
 		val id = pool.getId();
-		assertNotEquals(memoryPool.getId(), id,    "the id should not be the same");
-		assertTrue(memoryPool.compareTo(pool) < 0, "the id should be smaller");
-		assertTrue(pool.compareTo(memoryPool) > 0, "the id should be bigger");
-		MemoryPool.addMemoryPool(pool);
-		assertNotEquals(pool.getId(), id, "the id should have changed");
-		assertThrows(NullPointerException.class, () -> memoryPool.compareTo(null));
-		assertThrows(NullPointerException.class, () -> MemoryPool.addMemoryPool(null));
+		assertNotEquals(mempool.getId(), id);
+		assertTrue(mempool.compareTo(pool) < 0);
+		assertTrue(pool.compareTo(mempool) > 0);
+		assertTrue(MemoryPool.addMempool(pool));
+		assertFalse(MemoryPool.addMempool(null));
+		assertNotEquals(pool.getId(), id);
+		assertThrows(NullPointerException.class, () -> mempool.compareTo(null));
 	}
 
-	/** Deallocates the memory region that was allocated during {@link #allocate()}. */
+	/**
+	 * Checks that the {@link PacketBuffer}s can be freed using a {@link Collection}.
+	 * 
+	 * @see MemoryPool#addAll(java.util.Collection)
+	 */
+	@Test
+	@Order(3)
+	@DisplayName("The packet buffers can be freed in collection")
+	void addAll() {
+		buffers.clear();
+		while (!mempool.isEmpty()) {
+			buffers.add(mempool.pop());
+		}
+		buffers.add(0, null);
+		assertFalse(mempool.addAll(null));
+		assertFalse(mempool.addAll(List.of()));
+		assertFalse(mempool.addAll(Arrays.asList((PacketBuffer) null)));
+		assertTrue(mempool.addAll(buffers));
+		assertFalse(mempool.addAll(null));
+		assertFalse(mempool.addAll(List.of()));
+		assertFalse(mempool.addAll(Arrays.asList((PacketBuffer) null)));
+	}
+
+	/**
+	 * Deallocates the memory region that was allocated during {@link #allocate()}.
+	 * 
+	 * @see MemoryUtils#deallocate(long, long)
+	 */
 	@AfterAll
 	static void deallocate() {
 		MemoryUtils.deallocate(virtual, MemoryUtils.getHugepagesize());
