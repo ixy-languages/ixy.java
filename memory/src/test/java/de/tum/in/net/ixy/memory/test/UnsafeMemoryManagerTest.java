5cbeffa0d8ce01ffe3c67c4b103f4e0349cb9eab
Esaú García Sánchez-Torija <egasato@protonmail.com>
2019-06-10 18:05:40 +0200

    Finish UnsafeMemoryManager tests

--- memory/src/test/java/de/tum/in/net/ixy/memory/test/UnsafeMemoryManagerTest.java
+++ memory/src/test/java/de/tum/in/net/ixy/memory/test/UnsafeMemoryManagerTest.java
@@ -1,13 +1,10 @@
 package de.tum.in.net.ixy.memory.test;
 
 import de.tum.in.net.ixy.generic.IxyMemoryManager;
-import de.tum.in.net.ixy.memory.BuildConfig;
 import de.tum.in.net.ixy.memory.UnsafeMemoryManager;
 import lombok.val;
-import org.assertj.core.api.SoftAssertions;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DynamicTest;
@@ -18,7 +15,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestFactory;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
-import org.junit.jupiter.api.parallel.ResourceLock;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -29,7 +25,6 @@ import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Random;
-import java.util.regex.Pattern;
 import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
@@ -47,20 +42,15 @@ import static org.assertj.core.api.Assumptions.assumeThat;
  * @author Esaú García Sánchez-Torija
  */
 @DisplayName("UnsafeMemoryManager")
-@SuppressWarnings({"HardCodedStringLiteral", "DuplicateStringLiteralInspection", "ResultOfMethodCallIgnored"})
 final class UnsafeMemoryManagerTest extends AbstractMemoryManagerTest {
 
 	/** A cached instance of a pseudo-random number generator. */
 	private static final @NotNull Random random = new SecureRandom();
 
-	/** The cloned memory manager instance to test. */
-	private @Nullable IxyMemoryManager mmanagerClone;
-
 	// Creates an "UnsafeMemoryManager" instance
 	@BeforeEach
 	void setUp() {
 		mmanager = UnsafeMemoryManager.getSingleton();
-		mmanagerClone = (IxyMemoryManager) allocateInstance(UnsafeMemoryManager.class);
 	}
 
 	@Test
@@ -87,7 +77,6 @@ final class UnsafeMemoryManagerTest extends AbstractMemoryManagerTest {
 	 *
 	 * @author Esaú García Sánchez-Torija
 	 */
-	@SuppressWarnings("ResultOfMethodCallIgnored")
 	@Nested
 	final class Parameters {
 
@@ -96,6 +85,10 @@ final class UnsafeMemoryManagerTest extends AbstractMemoryManagerTest {
 		@DisabledIfOptimized
 		@Contract(value = " -> new", pure = true)
 		@NotNull Collection<@NotNull DynamicTest> exceptions() {
+			// Create a clone of the memory manager without calling the constructor, this way the Unsafe object is null
+			val mmanagerClone = (IxyMemoryManager) allocateInstance(mmanager.getClass());
+			assumeThat(mmanagerClone).isNotNull();
+			// Compute how many tests we will need based on the different combinations a method has
 			var expected = 0;
 			expected += 2 * 3 * 3 * 3; // get
 			expected += 2 * 3 * 3 * 3; // getVolatile
@@ -106,7 +99,6 @@ final class UnsafeMemoryManagerTest extends AbstractMemoryManagerTest {
 			expected += 3 * 3 * 3;     // allocate
 			expected += 3 * 3 * 2;     // free
 			Collection<DynamicTest> tests = new ArrayList<>(expected);
-
 			// Create the tests for get(Volatile)/put(Volatile)
 			long[] addresses = {0L, 1L};
 			int[] sizes = {-1, 0, 1};
@@ -120,25 +112,25 @@ final class UnsafeMemoryManagerTest extends AbstractMemoryManagerTest {
 								val buff = buffer == null ? "null" : String.format("byte[%d]", buffer.length);
 								var name = String.format("Parameters are checked for get(%d, %d, %s, %d)", address, size, buff, offset);
 								tests.add(DynamicTest.dynamicTest(name, () -> {
-									assumeManagers();
+									assumeThat(mmanager).isNotNull();
 									assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> mmanager.get(address, size, buffer, offset));
 									assertThatExceptionOfType(IllegalStateException.class).isThrownBy(() -> mmanagerClone.get(address, size, buffer, offset));
 								}));
 								name = String.format("Arguments are checked for getVolatile(%d, %d, %s, %d)", address, size, buff, offset);
 								tests.add(DynamicTest.dynamicTest(name, () -> {
-									assumeManagers();
+									assumeThat(mmanager).isNotNull();
 									assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> mmanager.getVolatile(address, size, buffer, offset));
 									assertThatExceptionOfType(IllegalStateException.class).isThrownBy(() -> mmanagerClone.getVolatile(address, size, buffer, offset));
 								}));
 								name = String.format("Arguments are checked for put(%d, %d, %s, %d)", address, size, buff, offset);
 								tests.add(DynamicTest.dynamicTest(name, () -> {
-									assumeManagers();
+									assumeThat(mmanager).isNotNull();
 									assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> mmanager.put(address, size, buffer, offset));
 									assertThatExceptionOfType(IllegalStateException.class).isThrownBy(() -> mmanagerClone.put(address, size, buffer, offset));
 								}));
 								name = String.format("Arguments are checked for putVolatile(%d, %d, %s, %d)", address, size, buff, offset);
 								tests.add(DynamicTest.dynamicTest(name, () -> {
-									assumeManagers();
+									assumeThat(mmanager).isNotNull();
 									assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> mmanager.putVolatile(address, size, buffer, offset));
 									assertThatExceptionOfType(IllegalStateException.class).isThrownBy(() -> mmanagerClone.putVolatile(address, size, buffer, offset));
 								}));
@@ -147,7 +139,6 @@ final class UnsafeMemoryManagerTest extends AbstractMemoryManagerTest {
 					}
 				}
 			}
-
 			// Create the tests for copy(Volatile)
 			addresses = new long[]{0L, 1L};
 			sizes = new int[]{-1, 0, 1};
@@ -157,13 +148,13 @@ final class UnsafeMemoryManagerTest extends AbstractMemoryManagerTest {
 						if (src == 0 || size < 0 || dest == 0) {
 							var name = String.format("Parameters are checked for copy(%d, %d, %d)", src, size, dest);
 							tests.add(DynamicTest.dynamicTest(name, () -> {
-								assumeManagers();
+								assumeThat(mmanager).isNotNull();
 								assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> mmanager.copy(src, size, dest));
 								assertThatExceptionOfType(IllegalStateException.class).isThrownBy(() -> mmanagerClone.copy(src, size, dest));
 							}));
 							name = String.format("Parameters are checked for copyVolatile(%d, %d, %d)", src, size, dest);
 							tests.add(DynamicTest.dynamicTest(name, () -> {
-								assumeManagers();
+								assumeThat(mmanager).isNotNull();
 								assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> mmanager.copyVolatile(src, size, dest));
 								assertThatExceptionOfType(IllegalStateException.class).isThrownBy(() -> mmanagerClone.copyVolatile(src, size, dest));
 							}));
@@ -171,7 +162,6 @@ final class UnsafeMemoryManagerTest extends AbstractMemoryManagerTest {
 					}
 				}
 			}
-
 			// Create the tests for allocate/free
 			addresses = new long[]{0L, 1L};
 			sizes = new int[]{-1, 0, 1};
@@ -183,7 +173,7 @@ final class UnsafeMemoryManagerTest extends AbstractMemoryManagerTest {
 						if (size == 0 || allocationType == null || layoutType == null) {
 							var name = String.format("Parameters are checked for allocate(%d, %s, %s)", size, allocationType, layoutType);
 							tests.add(DynamicTest.dynamicTest(name, () -> {
-								assumeManagers();
+								assumeThat(mmanager).isNotNull();
 								assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> mmanager.allocate(size, allocationType, layoutType));
 								assertThatExceptionOfType(IllegalStateException.class).isThrownBy(() -> mmanagerClone.allocate(size, allocationType, layoutType));
 							}));
@@ -193,7 +183,7 @@ final class UnsafeMemoryManagerTest extends AbstractMemoryManagerTest {
 						if (address == 0 || size == 0 || allocationType == null) {
 							val name = String.format("Parameters are checked for free(%d, %d, %s)", address, size, allocationType);
 							tests.add(DynamicTest.dynamicTest(name, () -> {
-								assumeManagers();
+								assumeThat(mmanager).isNotNull();
 								assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> mmanager.free(address, size, allocationType));
 								assertThatExceptionOfType(IllegalStateException.class).isThrownBy(() -> mmanagerClone.free(address, size, allocationType));
 							}));
@@ -201,93 +191,91 @@ final class UnsafeMemoryManagerTest extends AbstractMemoryManagerTest {
 					}
 				}
 			}
-
 			/// Add other tests that cannot be added with a loop
 			// For bytes
 			tests.add(DynamicTest.dynamicTest("Parameters are checked for getByte(0)", () -> {
-				assumeManagers();
+				assumeThat(mmanager).isNotNull();
 				assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> mmanager.getByte(0L));
 				assertThatExceptionOfType(IllegalStateException.class).isThrownBy(() -> mmanagerClone.getByte(0L));
 			}));
 			tests.add(DynamicTest.dynamicTest("Parameters are checked for getByteVolatile(0)", () -> {
-				assumeManagers();
+				assumeThat(mmanager).isNotNull();
 				assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> mmanager.getByteVolatile(0L));
 				assertThatExceptionOfType(IllegalStateException.class).isThrownBy(() -> mmanagerClone.getByteVolatile(0L));
 			}));
 			tests.add(DynamicTest.dynamicTest("Parameters are checked for putByte(0, 0)", () -> {
-				assumeManagers();
+				assumeThat(mmanager).isNotNull();
 				assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> mmanager.putByte(0L, (byte) 0));
 				assertThatExceptionOfType(IllegalStateException.class).isThrownBy(() -> mmanagerClone.putByte(0L, (byte) 0));
 			}));
 			tests.add(DynamicTest.dynamicTest("Parameters are checked for putByteVolatile(0, 0)", () -> {
-				assumeManagers();
+				assumeThat(mmanager).isNotNull();
 				assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> mmanager.putByteVolatile(0L, (byte) 0));
 				assertThatExceptionOfType(IllegalStateException.class).isThrownBy(() -> mmanagerClone.putByteVolatile(0L, (byte) 0));
 			}));
 			// For shorts
 			tests.add(DynamicTest.dynamicTest("Parameters are checked for getShort(0)", () -> {
-				assumeManagers();
+				assumeThat(mmanager).isNotNull();
 				assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> mmanager.getShort(0L));
 				assertThatExceptionOfType(IllegalStateException.class).isThrownBy(() -> mmanagerClone.getShort(0L));
 			}));
 			tests.add(DynamicTest.dynamicTest("Parameters are checked for getShortVolatile(0)", () -> {
-				assumeManagers();
+				assumeThat(mmanager).isNotNull();
 				assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> mmanager.getShortVolatile(0L));
 				assertThatExceptionOfType(IllegalStateException.class).isThrownBy(() -> mmanagerClone.getShortVolatile(0L));
 			}));
 			tests.add(DynamicTest.dynamicTest("Parameters are checked for putShort(0, 0)", () -> {
-				assumeManagers();
+				assumeThat(mmanager).isNotNull();
 				assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> mmanager.putShort(0L, (short) 0));
 				assertThatExceptionOfType(IllegalStateException.class).isThrownBy(() -> mmanagerClone.putShort(0L, (short) 0));
 			}));
 			tests.add(DynamicTest.dynamicTest("Parameters are checked for putShortVolatile(0, 0)", () -> {
-				assumeManagers();
+				assumeThat(mmanager).isNotNull();
 				assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> mmanager.putShortVolatile(0L, (short) 0));
 				assertThatExceptionOfType(IllegalStateException.class).isThrownBy(() -> mmanagerClone.putShortVolatile(0L, (short) 0));
 			}));
 			// For ints
 			tests.add(DynamicTest.dynamicTest("Parameters are checked for getInt(0)", () -> {
-				assumeManagers();
+				assumeThat(mmanager).isNotNull();
 				assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> mmanager.getInt(0L));
 				assertThatExceptionOfType(IllegalStateException.class).isThrownBy(() -> mmanagerClone.getInt(0L));
 			}));
 			tests.add(DynamicTest.dynamicTest("Parameters are checked for getIntVolatile(0)", () -> {
-				assumeManagers();
+				assumeThat(mmanager).isNotNull();
 				assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> mmanager.getIntVolatile(0L));
 				assertThatExceptionOfType(IllegalStateException.class).isThrownBy(() -> mmanagerClone.getIntVolatile(0L));
 			}));
 			tests.add(DynamicTest.dynamicTest("Parameters are checked for putInt(0, 0)", () -> {
-				assumeManagers();
+				assumeThat(mmanager).isNotNull();
 				assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> mmanager.putInt(0L, 0));
 				assertThatExceptionOfType(IllegalStateException.class).isThrownBy(() -> mmanagerClone.putInt(0L, 0));
 			}));
 			tests.add(DynamicTest.dynamicTest("Parameters are checked for putIntVolatile(0, 0)", () -> {
-				assumeManagers();
+				assumeThat(mmanager).isNotNull();
 				assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> mmanager.putIntVolatile(0L, 0));
 				assertThatExceptionOfType(IllegalStateException.class).isThrownBy(() -> mmanagerClone.putIntVolatile(0L, 0));
 			}));
 			// For longs
 			tests.add(DynamicTest.dynamicTest("Parameters are checked for getLong(0)", () -> {
-				assumeManagers();
+				assumeThat(mmanager).isNotNull();
 				assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> mmanager.getLong(0L));
 				assertThatExceptionOfType(IllegalStateException.class).isThrownBy(() -> mmanagerClone.getLong(0L));
 			}));
 			tests.add(DynamicTest.dynamicTest("Parameters are checked for getLongVolatile(0)", () -> {
-				assumeManagers();
+				assumeThat(mmanager).isNotNull();
 				assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> mmanager.getLongVolatile(0L));
 				assertThatExceptionOfType(IllegalStateException.class).isThrownBy(() -> mmanagerClone.getLongVolatile(0L));
 			}));
 			tests.add(DynamicTest.dynamicTest("Parameters are checked for putLong(0, 0)", () -> {
-				assumeManagers();
+				assumeThat(mmanager).isNotNull();
 				assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> mmanager.putLong(0L, 0L));
 				assertThatExceptionOfType(IllegalStateException.class).isThrownBy(() -> mmanagerClone.putLong(0L, 0L));
 			}));
 			tests.add(DynamicTest.dynamicTest("Parameters are checked for putLongVolatile(0, 0)", () -> {
-				assumeManagers();
+				assumeThat(mmanager).isNotNull();
 				assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> mmanager.putLongVolatile(0L, 0L));
 				assertThatExceptionOfType(IllegalStateException.class).isThrownBy(() -> mmanagerClone.putLongVolatile(0L, 0L));
 			}));
-
 			return tests;
 		}
 
@@ -345,20 +333,16 @@ final class UnsafeMemoryManagerTest extends AbstractMemoryManagerTest {
 				// Test the addresses with the correct data type
 				switch (bytes) {
 					case Byte.BYTES:
-						aligned.peek(address -> testWrite(address, (byte) rand1, false))
-								.forEach(address -> testWrite(address, (byte) rand2, true));
+						aligned.peek(address -> testWrite(address, (byte) rand1, false)).forEach(address -> testWrite(address, (byte) rand2, true));
 						break;
 					case Short.BYTES:
-						aligned.peek(address -> testWrite(address, (short) rand1, false))
-								.forEach(address -> testWrite(address, (short) rand2, true));
+						aligned.peek(address -> testWrite(address, (short) rand1, false)).forEach(address -> testWrite(address, (short) rand2, true));
 						break;
 					case Integer.BYTES:
-						aligned.peek(address -> testWrite(address, (int) rand1, false))
-								.forEach(address -> testWrite(address, (int) rand2, true));
+						aligned.peek(address -> testWrite(address, (int) rand1, false)).forEach(address -> testWrite(address, (int) rand2, true));
 						break;
 					case Long.BYTES:
-						aligned.peek(address -> testWrite(address, rand1, false))
-								.forEach(address -> testWrite(address, rand2, true));
+						aligned.peek(address -> testWrite(address, rand1, false)).forEach(address -> testWrite(address, rand2, true));
 						break;
 					default:
 						fail("the number of bytes makes no sense");
@@ -477,73 +461,6 @@ final class UnsafeMemoryManagerTest extends AbstractMemoryManagerTest {
 		val virt = assumeAllocate(1);
 		assertThatExceptionOfType(UnsupportedOperationException.class).as("Translation").isThrownBy(() -> mmanager.virt2phys(virt));
 		mmanager.free(virt, 1, AllocationType.STANDARD);
-	}
-
-	@Test
-	@ResourceLock(BuildConfig.LOCK)
-	@DisplayName("The equals(Object) method works as expected")
-	void equalsTest() {
-		assumeManagers();
-		// Assert as many different cases as possible
-		val softly = new SoftAssertions();
-		softly.assertThat(mmanager).isNotEqualTo(null);
-		softly.assertThat(mmanagerClone).isNotEqualTo(null);
-		softly.assertThat(mmanager).isNotEqualTo(softly);
-		softly.assertThat(mmanagerClone).isNotEqualTo(softly);
-		softly.assertThat(mmanager).isEqualTo(mmanager);
-		softly.assertThat(mmanagerClone).isEqualTo(mmanagerClone);
-		softly.assertThat(mmanager).isNotEqualTo(mmanagerClone);
-		softly.assertThat(mmanagerClone).isNotEqualTo(mmanager);
-		// Do nasty things to get 100% coverage
-		val unsafeField = getDeclaredField(UnsafeMemoryManager.class, "unsafe");
-		if (unsafeField != null) {
-			unsafeField.setAccessible(true);
-			val unsafe = fieldGet(unsafeField, mmanager);
-			fieldSet(unsafeField, mmanagerClone, unsafe);
-			softly.assertThat(mmanager).isEqualTo(mmanagerClone);
-			softly.assertThat(mmanagerClone).isEqualTo(mmanager);
-			fieldSet(unsafeField, mmanagerClone, null);
-			unsafeField.setAccessible(false);
-		}
-		softly.assertAll();
-	}
-
-	@Test
-	@ResourceLock(BuildConfig.LOCK)
-	@DisplayName("The hashCode() method works as expected")
-	void hashCodeTest() {
-		assumeManagers();
-		// Assert as many different cases as possible
-		val softly = new SoftAssertions();
-		softly.assertThat(mmanagerClone.hashCode()).as("Hash code").isNotEqualTo(mmanager.hashCode());
-		// Do nasty things to get 100% coverage
-		val unsafeField = getDeclaredField(UnsafeMemoryManager.class, "unsafe");
-		if (unsafeField != null) {
-			unsafeField.setAccessible(true);
-			val unsafe = fieldGet(unsafeField, mmanager);
-			fieldSet(unsafeField, mmanagerClone, unsafe);
-			softly.assertThat(mmanagerClone.hashCode()).as("Hash code").isEqualTo(mmanager.hashCode());
-			fieldSet(unsafeField, mmanagerClone, null);
-			unsafeField.setAccessible(false);
-		}
-		softly.assertAll();
-	}
-
-	@Test
-	@SuppressWarnings("HardcodedFileSeparator")
-	@DisplayName("The string representation is correct")
-	void toStringTest() {
-		assumeThat(mmanager).isNotNull();
-		val genericPattern = "^%s\\([a-zA-Z]+=(sun\\.misc\\.)?Unsafe@[0-9a-z]+\\)$";
-		val specificPattern = String.format(genericPattern, UnsafeMemoryManager.class.getSimpleName());
-		val pattern = Pattern.compile(specificPattern);
-		assertThat(mmanager.toString()).as("String representation").matches(pattern);
-	}
-
-	/** Assumes the managers {@link #mmanager} and {@link #mmanagerClone} are not {@code null}. */
-	private void assumeManagers() {
-		assumeThat(mmanager).isNotNull();
-		assumeThat(mmanagerClone).isNotNull();
 	}
 
 	/**
