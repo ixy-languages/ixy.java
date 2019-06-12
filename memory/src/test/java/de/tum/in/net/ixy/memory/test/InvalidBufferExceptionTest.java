928ac6e7fd78ccd3b0d04d0b1b100d629f9802ed
Esaú García Sánchez-Torija <egasato@protonmail.com>
2019-06-12 19:29:18 +0200

    Improve coverage of exceptions

--- memory/src/test/java/de/tum/in/net/ixy/memory/test/InvalidBufferExceptionTest.java
+++ memory/src/test/java/de/tum/in/net/ixy/memory/test/InvalidBufferExceptionTest.java
@@ -1,10 +1,15 @@
 package de.tum.in.net.ixy.memory.test;
 
 import de.tum.in.net.ixy.memory.InvalidBufferException;
+import lombok.val;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
+
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 /**
  * Tests the class {@link InvalidBufferException}.
@@ -14,10 +19,20 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 final class InvalidBufferExceptionTest {
 
 	@Test
+	@DisabledIfOptimized
 	@DisplayName("Null or blank constructor fails")
-	void constructor() {
+	void exceptions() {
 		assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new InvalidBufferException(null));
 		assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new InvalidBufferException(""));
+	}
+
+	@Test
+	@DisplayName("The constructor works as expected")
+	void constructor() {
+		val array = new byte[8];
+		new SecureRandom().nextBytes(array);
+		val parameter = new String(array, StandardCharsets.UTF_8);
+		assertDoesNotThrow(() -> new InvalidBufferException(parameter));
 	}
 
 }
