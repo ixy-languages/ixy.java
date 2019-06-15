53827372426706068084f804608e22040f654402
Esaú García Sánchez-Torija <egasato@protonmail.com>
2019-06-15 22:14:58 +0200

    Add package information of PCI module

---
+++ pci/src/main/java/de/tum/in/net/ixy/pci/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Contains the implementation of the PCI device specifications from {@link de.tum.in.net.ixy.generic}.
+ *
+ * @author Esaú García Sánchez-Torija
+ */
+package de.tum.in.net.ixy.pci;
