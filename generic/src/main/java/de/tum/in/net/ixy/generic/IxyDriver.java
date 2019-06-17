81f3e80d7d44ff18e7f2c6e17f9a9361872e3af0
Esaú García Sánchez-Torija <egasato@protonmail.com>
2019-06-17 03:39:13 +0200

    Add plugin architecture

--- generic/src/main/java/de/tum/in/net/ixy/generic/IxyDriver.java
+++ generic/src/main/java/de/tum/in/net/ixy/generic/IxyDriver.java
@@ -1,13 +1,14 @@
 package de.tum.in.net.ixy.generic;
 
 import org.jetbrains.annotations.NotNull;
+import org.pf4j.ExtensionPoint;
 
 /**
  * Ixy's driver specification.
  *
  * @author Esaú García Sánchez-Torija
  */
-public interface IxyDriver {
+public interface IxyDriver extends ExtensionPoint {
 
 	@NotNull IxyDevice getDevice(@NotNull String device, @NotNull String driver);
 
