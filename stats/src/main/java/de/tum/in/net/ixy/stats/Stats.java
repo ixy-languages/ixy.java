f30330d360a02d7cb7fa4df7d6839b365dd0439e
Esaú García Sánchez-Torija <egasato@protonmail.com>
2019-06-17 03:23:15 +0200

    Finish stats

--- stats/src/main/java/de/tum/in/net/ixy/stats/Stats.java
+++ stats/src/main/java/de/tum/in/net/ixy/stats/Stats.java
@@ -1,18 +1,24 @@
 package de.tum.in.net.ixy.stats;
 
 import de.tum.in.net.ixy.generic.BuildConfig;
-import de.tum.in.net.ixy.generic.InvalidNullParameterException;
 import de.tum.in.net.ixy.generic.InvalidSizeException;
 import de.tum.in.net.ixy.generic.IxyPciDevice;
 import de.tum.in.net.ixy.generic.IxyStats;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Simple implementation of Ixy's stats specification.
+ *
+ * @author Esaú García Sánchez-Torija
+ */
 @Slf4j
+@RequiredArgsConstructor(staticName = "of")
 @ToString(onlyExplicitlyIncluded = true, doNotUseGetters = true)
 @EqualsAndHashCode(onlyExplicitlyIncluded = true, doNotUseGetters = true, callSuper = true)
 public final class Stats extends IxyStats {
@@ -43,17 +49,6 @@ public final class Stats extends IxyStats {
 	@ToString.Include(name = "tx_bytes", rank = 1)
 	@Getter(onMethod_ = {@Contract(pure = true)})
 	private long txBytes;
-
-	/**
-	 * Creates a new stats instance.
-	 *
-	 * @param device The device being tracked.
-	 */
-	public Stats(@NotNull IxyPciDevice device) {
-		if (BuildConfig.DEBUG) log.debug("Tracking stats of {}", device);
-		if (!BuildConfig.OPTIMIZED && device == null) throw new InvalidNullParameterException("device");
-		this.device = device;
-	}
 
 	//////////////////////////////////////////////// OVERRIDDEN METHODS ////////////////////////////////////////////////
 
---
+++ stats/src/test/java/de/tum/in/net/ixy/stats/test/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Package containing all the JUnit 5 tests of the package {@link de.tum.in.net.ixy.stats}.
+ *
+ * @author Esaú García Sánchez-Torija
+ */
+package de.tum.in.net.ixy.stats.test;
