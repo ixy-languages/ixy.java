bd7213b76d02441524770c26141a5ca0e775b98f
Esaú García Sánchez-Torija <egasato@protonmail.com>
2019-06-22 15:59:03 +0200

    Fix exceptions scopes and add tests

--- generic/src/main/java/de/tum/in/net/ixy/generic/InvalidSizeException.java
+++ generic/src/main/java/de/tum/in/net/ixy/generic/InvalidSizeException.java
@@ -8,7 +8,7 @@ import org.jetbrains.annotations.NotNull;
  *
  * @author Esaú García Sánchez-Torija
  */
-public final class InvalidSizeException extends IllegalArgumentException {
+final class InvalidSizeException extends IllegalArgumentException {
 
 	/** Serial used for serialization purposes. */
 	private static final long serialVersionUID = -8148271958732546981L;
@@ -19,7 +19,7 @@ public final class InvalidSizeException extends IllegalArgumentException {
 	 * @param parameter The name of the parameter that is wrong.
 	 */
 	@Contract("null -> fail")
-	public InvalidSizeException(@NotNull String parameter) {
+	InvalidSizeException(@NotNull String parameter) {
 		super(String.format("The parameter '%s' is an invalid size", parameter));
 		if (!BuildConfig.OPTIMIZED && (parameter == null || parameter.isBlank())) {
 			throw new IllegalArgumentException("The exception message could not be constructed correctly");
---
+++ generic/src/test/java/de/tum/in/net/ixy/generic/InvalidSizeExceptionTest.java
@@ -0,0 +1,49 @@
+package de.tum.in.net.ixy.generic;
+
+import lombok.val;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+
+import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+/**
+ * Tests the class {@link InvalidSizeException}.
+ *
+ * @author Esaú García Sánchez-Torija
+ */
+@Execution(ExecutionMode.CONCURRENT)
+@DisplayName("InvalidSizeException")
+final class InvalidSizeExceptionTest {
+
+	@Nested
+	@DisabledIfOptimized
+	@DisplayName("InvalidSizeException (Parameters)")
+	final class Parameters {
+
+		@Test
+		@DisabledIfOptimized
+		@DisplayName("Null or blank constructor fails")
+		void exceptions() {
+			assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new InvalidSizeException(null));
+			assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new InvalidSizeException(""));
+		}
+
+	}
+
+	@Test
+	@DisplayName("The constructor works as expected")
+	void constructor() {
+		val array = new byte[8];
+		new SecureRandom().nextBytes(array);
+		val parameter = new String(array, StandardCharsets.UTF_8);
+		assertDoesNotThrow(() -> new InvalidSizeException(parameter));
+	}
+
+}
